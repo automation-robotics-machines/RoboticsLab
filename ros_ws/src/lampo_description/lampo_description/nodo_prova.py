728c8e74ea50e0d32203b69cb36fc3463dcb8652
Stefano Mutti <stefano.mutti@supsi.ch>
2025-12-05 13:10:07 +0100

    S

--- ros_ws/src/lampo_description/lampo_description/nodo_prova.py
+++ ros_ws/src/lampo_description/lampo_description/nodo_prova.py
@@ -4,7 +4,7 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
-
+from geometry_msgs.msg import Twist
 
 
 
@@ -12,16 +12,16 @@ class NodoMio(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
 
-        # self.publisher_   = self.create_publisher(String, 'topic', 10)
+        self.publisher_   = self.create_publisher(Twist, '/r1_/cmd_vel', 10)
         self.subscription = self.create_subscription(
             Odometry,
             '/r1_/odom',
             self.listener_callback,
             10)
         
-        # timer_period = 0.5  # seconds
-        # self.timer = self.create_timer(timer_period, self.timer_callback)
-        # self.i = 0
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
 
     def listener_callback(self, msg : Odometry):
         self.get_logger().info('x: "%f"' % msg.pose.pose.position.x)
@@ -30,11 +30,9 @@ class NodoMio(Node):
         self.get_logger().info('\n\n')
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = Twist()
+        msg.linear.x = 0.2
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
 
 
 def main(args=None):
